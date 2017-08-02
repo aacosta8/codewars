61ab34e56d9fbdca239220c8667037efff35496f
Alex <aacosta8@eafit.edu.co>
2017-08-02 10:01:41 -0500

    Two exercices more

---
+++ greet.rb
@@ -0,0 +1,6 @@
+def greet(name)
+return nil if  name == nil or name.empty?
+
+"hello " << name << "!"
+
+end
---
+++ invert.rb
@@ -0,0 +1,7 @@
+def invert(list)
+
+  for i in (0..list.size-1) do
+    list[i] = list[i] * - 1
+  end
+  list
+end
